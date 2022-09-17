7be5578868838c62a25f336db879d518fe30a29b
ji-mmyliu <jimmyjcl753@gmail.com>
2022-09-17 12:14:45 -0400

    Add many components

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,26 +1,126 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route, useNavigate, NavigateFunction } from "react-router-dom";
 
-import { NavBar } from "./nav";
 import { NotFound } from "../notfound";
 
 import "../index.scss";
 import { Home } from "../home";
 
-export const _App = (): JSX.Element => {
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Room } from "../room";
+
+import { Props, navItems, NavLink } from "../../util/nav";
+
+const drawerWidth = 240;
+
+export const _App = (props: Props) => {
     const nav: NavigateFunction = useNavigate();
 
-    return (
-        <div className="page">
-            <NavBar />
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-            <div className="router-outlet">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </div>
-        </div>
+    const handleDrawerToggle = (): void => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer: JSX.Element = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                RoundTable
+            </Typography>
+            <Divider />
+            <List>
+                {navItems.map((item: NavLink) => (
+                    <ListItem key={item.text} disablePadding>
+                        <ListItemButton onClick={() => { nav(item.href) }} sx={{ textAlign: "center" }}>
+                            <ListItemText primary={item.text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box sx={{ display: "flex" }}>
+            <AppBar component="nav" color="secondary">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: "none" } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+                    >
+                        RoundTable
+                    </Typography>
+                    <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                        {navItems.map((item: NavLink) => (
+                            <Button key={item.text} onClick={() => { nav(item.href) }} sx={{ color: "#fff" }}>
+                                {item.text}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <Box component="nav">
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true,
+                    }}
+                    sx={{
+                        display: { xs: "block", sm: "none" },
+                        "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+            <Box component="main" sx={{ p: 3 }}>
+                <Toolbar />
+
+                {/* Router Outlet */}
+                <div className="page">
+                    <div className="router-outlet">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/room" element={<Room />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </div>
+                </div>
+            </Box>
+        </Box>
     );
 }
 
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,7 +1,66 @@
 import * as React from "react";
 
-export const Home = () => {
+import { Box, Typography } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { makeStyles } from "@mui/material/styles";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Container from '@mui/material/Container';
+import { default as axios } from "axios";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+
+export const Home = (): JSX.Element => {
+    const joinRef = React.useRef("");
+    const nav: NavigateFunction = useNavigate();
+
+    const onJoin = (): void => {
+        // @ts-ignore
+        const roomCode: string = joinRef.current.value;
+        axios.post("/api/leyangimplementfaster", {
+            "username": "baf",
+            "code": roomCode,
+        }).then((res): void => {
+            nav(`/table?id=${res.data.id}`);
+        }).catch((err): void => {
+            console.log("Error occured when joining room:", err);
+        });
+    }
+
     return (
-        <p>Homepage.</p>
+        <div id="homepage">
+            <Container maxWidth="sm">
+                <Stack spacing={10} direction="row">
+                    <div className="home-header">
+                        <Typography variant="h1">
+                            RoundTable
+                        </Typography>
+
+                        <div className="subtitle">
+                            <Typography variant="subtitle1" fontSize={24} gutterBottom>
+                                A round table for well-rounded people!
+                            </Typography>
+                        </div>
+
+                        <Stack spacing={1} direction="row">
+                            <Box bgcolor="white">
+                                <TextField
+                                    inputRef={joinRef}
+                                    label="Enter room join code:"
+                                    variant="filled"
+                                />
+                            </Box>
+                            <Button variant="contained" onClick={onJoin}>Join room!</Button>
+                        </Stack>
+                    </div>
+                    <img className="table-image" src={"img/roundtable1.png"} />
+                </Stack>
+            </Container>
+        </div>
     );
 }
