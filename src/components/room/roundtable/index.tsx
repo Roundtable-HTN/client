2efe722aa146b4997bb4a9fc481ca01ed74603db
Patrick Lin <apocalypsecalculator@gmail.com>
2022-09-18 05:03:09 -0400

    add stuff

--- src/components/room/roundtable/index.tsx
+++ src/components/room/roundtable/index.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 
 export const RoundTable = (): JSX.Element => {
+    React.useEffect(() => {
+        let script = document.createElement('script');
+        script.src = "/js/index.js";
+        document.body.appendChild(script);
+        let script2 = document.createElement('script');
+        script2.src = "/js/plugin1.js";
+        document.body.appendChild(script2);
+        return () => {
+            document.body.removeChild(script);
+            document.body.removeChild(script2);
+        }
+    }, [])
     return (
-        <canvas>
-
-        </canvas>
+        <div id={"canvasContainer"} style={{ width: "300px" }}>
+        </div>
     );
 }
--- src/components/room/sidenav/index.tsx
+++ src/components/room/sidenav/index.tsx
@@ -4,6 +4,7 @@ import { HomeIcon } from "../../../util/icons";
 import ExtensionIcon from '@mui/icons-material/Extension';
 import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
 import { NavigateFunction, useNavigate } from "react-router-dom";
+import { SocketContext } from "../socket_context";
 
 const SideNavItem = (props: { text: string, click: () => void, icon: JSX.Element }): JSX.Element => {
     return (
@@ -37,12 +38,17 @@ export const SideNav = (): JSX.Element => {
 }
 
 const plugins = (): Array<JSX.Element> => {
+    const ctx = React.useContext(SocketContext);
     return (
         [
             <SideNavItem
                 key="1"
                 text="Add Connect 4"
-                click={(): void => { }}
+                click={(): void => {
+                    // @ts-ignore
+                    createPluginConnectFour();
+                    ctx.value.socket.emit("plugin_instance_create", 1);
+                }}
                 icon={<VideogameAssetIcon />}
             />
         ]
